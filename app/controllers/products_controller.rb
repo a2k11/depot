ed672e26cf6b1645ab3f6b40634833bf911b4b32
a2k11 <adam.kaewell@gmail.com>
2014-11-02 22:45:19 -0500

    Added an edit page to update the inventory Added a show page to show off each individual book Made some tests and they don't work just quite yet

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,11 +9,11 @@ class ProductsController < ApplicationController
   end
 
   def show
-
+    @product = Product.find(params[:id])
   end
 
   def edit
-
+    @product = Product.find(params[:id])
   end
 
   def create
@@ -26,10 +26,21 @@ class ProductsController < ApplicationController
   end
 
   def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
 
+    redirect_to products_path
   end
   
   def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+
+    if @product.save
+      redirect_to products_path
+    else
+      render :edit 
+    end
 
   end
 
